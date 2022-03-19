303663681ce3eebbc0d662f9c183a38101e235dd
SpaceWanWan <gandalfsugoi@gmail.com>
2022-03-18 22:52:51 -0600

    About, Skills and Work added

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,21 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+let mixerPortfolio = mixitup('.work_container', {
+  selectors: {
+      target: '.work_card'
+  },
+  animation: {
+      duration: 300
+  }
+});
+
+const linkWork = document.querySelectorAll('.work_item')
+
+function activeWork(){
+  linkWork.forEach(l=> l.classList.remove('active-work'))
+  this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
